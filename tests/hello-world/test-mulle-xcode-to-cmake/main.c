dbfb1c93b04d673820b685c768ef6e4d0f899c2f
Ray Donnelly <mingw.android@gmail.com>
2021-03-10 23:17:21 +0100

    Add simple hello-world test xcodeproject

---
+++ tests/hello-world/test-mulle-xcode-to-cmake/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test-mulle-xcode-to-cmake
+//
+//  Created by rdonnelly on 3/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
