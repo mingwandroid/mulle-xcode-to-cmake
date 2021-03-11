2ead944570a6a32f665ec2a8c688f802682b0f6b
Ray Donnelly <mingw.android@gmail.com>
2021-03-11 01:17:33 +0100

    VERBOSE=1

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 pushd hello-world
-  mulle-xcode-to-cmake -2 -b -d -i  export test-mulle-xcode-to-cmake.xcodeproj/project.pbxproj > test-mulle-xcode-to-cmake.xcodeproj/CMakeLists.txt
-  mulle-xcode-to-cmake -2 -b -d -i sexport test-mulle-xcode-to-cmake.xcodeproj/project.pbxproj > test-mulle-xcode-to-cmake.xcodeproj/CMakeSourcesAndHeaders.txt
+  VERBOSE=1 mulle-xcode-to-cmake -2 -b -d -i  export test-mulle-xcode-to-cmake.xcodeproj/project.pbxproj > test-mulle-xcode-to-cmake.xcodeproj/CMakeLists.txt
+  VERBOSE=1 mulle-xcode-to-cmake -2 -b -d -i sexport test-mulle-xcode-to-cmake.xcodeproj/project.pbxproj > test-mulle-xcode-to-cmake.xcodeproj/CMakeSourcesAndHeaders.txt
   pushd test-mulle-xcode-to-cmake.xcodeproj
     cmake .
   popd
